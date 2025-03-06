2878be9d70c6993745ce0bdc01039244c9a7f749
McMelonTV <mcmelon@nodiumhosting.com>
2025-03-06 03:06:26 +0100

    fix lag

--- src/main/java/ing/boykiss/gifrender/ModMain.java
+++ src/main/java/ing/boykiss/gifrender/ModMain.java
@@ -1,132 +1,28 @@
 package ing.boykiss.gifrender;
 
-import com.madgag.gif.fmsware.GifDecoder;
-import com.mojang.blaze3d.systems.RenderSystem;
-import ing.boykiss.gifrender.mixin.AccessorNativeImage;
+import ing.boykiss.gifrender.gui.GifDrawable;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.*;
-import net.minecraft.client.texture.NativeImage;
-import net.minecraft.client.texture.NativeImageBackedTexture;
-import net.minecraft.resource.Resource;
-import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
-import org.jetbrains.annotations.NotNull;
-import org.joml.Matrix4f;
-import org.lwjgl.system.MemoryUtil;
 
-import java.awt.image.BufferedImage;
-import java.awt.image.ColorModel;
-import java.awt.image.DataBuffer;
-import java.awt.image.WritableRaster;
-import java.io.IOException;
-import java.nio.IntBuffer;
-import java.util.HashMap;
 import java.util.Optional;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class ModMain implements ClientModInitializer {
 
-    boolean init = false;
-    MinecraftClient client = MinecraftClient.getInstance();
-    ResourceManager rm = null;
-    Optional<Resource> optionalResource = Optional.empty();
-    Resource resource = null;
-    GifDecoder decoder = new GifDecoder();
-    HashMap<Integer, Identifier> textures = new HashMap<>();
-
-    // yoinked from https://github.com/0x3C50/Renderer/blob/master/src/main/java/me/x150/renderer/util/RendererUtils.java#L166
-    public static @NotNull NativeImageBackedTexture bufferedImageToNIBT(@NotNull BufferedImage bi) {
-        // argb from BufferedImage is little endian, alpha is actually where the `a` is in the label
-        // rgba from NativeImage (and by extension opengl) is big endian, alpha is on the other side (abgr)
-        // thank you opengl
-        int ow = bi.getWidth();
-        int oh = bi.getHeight();
-        NativeImage image = new NativeImage(NativeImage.Format.RGBA, ow, oh, false);
-        @SuppressWarnings("DataFlowIssue") long ptr = ((AccessorNativeImage) (Object) image).getPointer();
-        IntBuffer backingBuffer = MemoryUtil.memIntBuffer(ptr, image.getWidth() * image.getHeight());
-        int off = 0;
-        Object _d;
-        WritableRaster raster = bi.getRaster();
-        ColorModel colorModel = bi.getColorModel();
-        int nbands = raster.getNumBands();
-        int dataType = raster.getDataBuffer().getDataType();
-        _d = switch (dataType) {
-            case DataBuffer.TYPE_BYTE -> new byte[nbands];
-            case DataBuffer.TYPE_USHORT -> new short[nbands];
-            case DataBuffer.TYPE_INT -> new int[nbands];
-            case DataBuffer.TYPE_FLOAT -> new float[nbands];
-            case DataBuffer.TYPE_DOUBLE -> new double[nbands];
-            default -> throw new IllegalArgumentException("Unknown data buffer type: " + dataType);
-        };
-
-        for (int y = 0; y < oh; y++) {
-            for (int x = 0; x < ow; x++) {
-                raster.getDataElements(x, y, _d);
-                int a = colorModel.getAlpha(_d);
-                int r = colorModel.getRed(_d);
-                int g = colorModel.getGreen(_d);
-                int b = colorModel.getBlue(_d);
-                int abgr = a << 24 | b << 16 | g << 8 | r;
-                backingBuffer.put(abgr);
-            }
-        }
-        NativeImageBackedTexture tex = new NativeImageBackedTexture(image);
-        tex.upload();
-        return tex;
-    }
-
     @Override
     public void onInitializeClient() {
+        AtomicReference<Optional<GifDrawable>> gifDrawable = new AtomicReference<>(Optional.empty());
+
+        ClientLifecycleEvents.CLIENT_STARTED.register((client) -> {
+            if (gifDrawable.get().isEmpty()) {
+                gifDrawable.set(Optional.of(new GifDrawable(Identifier.of("gifrender:textures/gif.gif"), 0, 0, 30, 50)));
+            }
+        });
+
         HudRenderCallback.EVENT.register((context, tickDelta) -> {
-            if (rm == null) {
-                rm = client.getResourceManager();
-            }
-            if (rm != null && !init) {
-                optionalResource = rm.getResource(Identifier.of("gifrender", "textures/gif.gif"));
-                resource = optionalResource.orElse(null);
-                if (resource == null) {
-                    return;
-                }
-                try {
-                    decoder.read(resource.getInputStream());
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-                init = true;
-            }
-
-            int frameCount = decoder.getFrameCount();
-            int frame = (int) (System.currentTimeMillis() / 100) % frameCount;
-
-            if (!textures.containsKey(frame)) {
-                BufferedImage image = decoder.getFrame(frame);
-                NativeImageBackedTexture texture = bufferedImageToNIBT(image);
-                Identifier i = MinecraftClient.getInstance().getTextureManager().registerDynamicTexture("gifrender", texture);
-                textures.put(frame, i);
-            }
-
-            Identifier i = textures.get(frame);
-            MinecraftClient.getInstance().getTextureManager().bindTexture(i);
-
-            Matrix4f matrix = context.getMatrices().peek().getPositionMatrix();
-
-            int x = 0;
-            int y = 0;
-            int width = 100;
-            int height = 100;
-
-            BufferBuilder bufferBuilder = Tessellator.getInstance().begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
-            bufferBuilder.vertex(matrix, x, y + height, 0).texture(0, 1);
-            bufferBuilder.vertex(matrix, x + width, y + height, 0).texture(1, 1);
-            bufferBuilder.vertex(matrix, x + width, y, 0).texture(1, 0);
-            bufferBuilder.vertex(matrix, x, y, 0).texture(0, 0);
-
-            RenderSystem.setShader(GameRenderer::getPositionTexProgram);
-            RenderSystem.setShaderTexture(0, i);
-
-            BuiltBuffer buffer = bufferBuilder.end();
-            BufferRenderer.drawWithGlobalProgram(buffer);
+            gifDrawable.get().ifPresent(gif -> gif.render(context, 0, 0, 0));
         });
     }
 }
---
+++ src/main/java/ing/boykiss/gifrender/gui/GifDrawable.java
@@ -0,0 +1,128 @@
+package ing.boykiss.gifrender.gui;
+
+import com.madgag.gif.fmsware.GifDecoder;
+import com.mojang.blaze3d.systems.RenderSystem;
+import ing.boykiss.gifrender.mixin.AccessorNativeImage;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.gui.Drawable;
+import net.minecraft.client.render.*;
+import net.minecraft.client.texture.NativeImage;
+import net.minecraft.client.texture.NativeImageBackedTexture;
+import net.minecraft.resource.Resource;
+import net.minecraft.resource.ResourceManager;
+import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
+import org.joml.Matrix4f;
+import org.lwjgl.system.MemoryUtil;
+
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.DataBuffer;
+import java.awt.image.WritableRaster;
+import java.nio.IntBuffer;
+import java.util.HashMap;
+import java.util.Optional;
+
+public class GifDrawable implements Drawable {
+    private final int x;
+    private final int y;
+    private final int width;
+    private final int height;
+    private final int frameCount;
+    private final GifDecoder gifDecoder = new GifDecoder();
+    private final HashMap<Integer, Identifier> frameTextures = new HashMap<>();
+
+    public GifDrawable(Identifier gif, int x, int y, int width, int height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+
+        MinecraftClient client = MinecraftClient.getInstance();
+        ResourceManager rm = client.getResourceManager();
+        if (rm == null) {
+            throw new IllegalStateException("ResourceManager is null");
+        }
+        Optional<Resource> optionalResource = rm.getResource(gif);
+        if (optionalResource.isEmpty()) {
+            throw new IllegalStateException("Resource not found: " + gif);
+        }
+        Resource gifResource = optionalResource.get();
+        try {
+            gifDecoder.read(gifResource.getInputStream());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        frameCount = gifDecoder.getFrameCount();
+    }
+
+    // yoinked from https://github.com/0x3C50/Renderer/blob/master/src/main/java/me/x150/renderer/util/RendererUtils.java#L166
+    private static @NotNull NativeImageBackedTexture bufferedImageToNIBT(@NotNull BufferedImage bi) {
+        // argb from BufferedImage is little endian, alpha is actually where the `a` is in the label
+        // rgba from NativeImage (and by extension opengl) is big endian, alpha is on the other side (abgr)
+        // thank you opengl
+        int ow = bi.getWidth();
+        int oh = bi.getHeight();
+        NativeImage image = new NativeImage(NativeImage.Format.RGBA, ow, oh, false);
+        @SuppressWarnings("DataFlowIssue") long ptr = ((AccessorNativeImage) (Object) image).getPointer();
+        IntBuffer backingBuffer = MemoryUtil.memIntBuffer(ptr, image.getWidth() * image.getHeight());
+        int off = 0;
+        Object _d;
+        WritableRaster raster = bi.getRaster();
+        ColorModel colorModel = bi.getColorModel();
+        int nbands = raster.getNumBands();
+        int dataType = raster.getDataBuffer().getDataType();
+        _d = switch (dataType) {
+            case DataBuffer.TYPE_BYTE -> new byte[nbands];
+            case DataBuffer.TYPE_USHORT -> new short[nbands];
+            case DataBuffer.TYPE_INT -> new int[nbands];
+            case DataBuffer.TYPE_FLOAT -> new float[nbands];
+            case DataBuffer.TYPE_DOUBLE -> new double[nbands];
+            default -> throw new IllegalArgumentException("Unknown data buffer type: " + dataType);
+        };
+
+        for (int y = 0; y < oh; y++) {
+            for (int x = 0; x < ow; x++) {
+                raster.getDataElements(x, y, _d);
+                int a = colorModel.getAlpha(_d);
+                int r = colorModel.getRed(_d);
+                int g = colorModel.getGreen(_d);
+                int b = colorModel.getBlue(_d);
+                int abgr = a << 24 | b << 16 | g << 8 | r;
+                backingBuffer.put(abgr);
+            }
+        }
+        NativeImageBackedTexture tex = new NativeImageBackedTexture(image);
+        tex.upload();
+        return tex;
+    }
+
+    public void render(DrawContext context, int mouseX, int mouseY, float delta) {
+        int frame = (int) (System.currentTimeMillis() / 100) % frameCount;
+
+        if (!frameTextures.containsKey(frame)) {
+            BufferedImage image = gifDecoder.getFrame(frame);
+            NativeImageBackedTexture texture = bufferedImageToNIBT(image);
+            Identifier i = MinecraftClient.getInstance().getTextureManager().registerDynamicTexture("gifrender", texture);
+            frameTextures.put(frame, i);
+        }
+
+        Identifier i = frameTextures.get(frame);
+        MinecraftClient.getInstance().getTextureManager().bindTexture(i);
+
+        Matrix4f matrix = context.getMatrices().peek().getPositionMatrix();
+
+        BufferBuilder bufferBuilder = Tessellator.getInstance().begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_TEXTURE);
+        bufferBuilder.vertex(matrix, x, y + height, 0).texture(0, 1);
+        bufferBuilder.vertex(matrix, x + width, y + height, 0).texture(1, 1);
+        bufferBuilder.vertex(matrix, x + width, y, 0).texture(1, 0);
+        bufferBuilder.vertex(matrix, x, y, 0).texture(0, 0);
+
+        RenderSystem.setShader(GameRenderer::getPositionTexProgram);
+        RenderSystem.setShaderTexture(0, i);
+
+        BuiltBuffer buffer = bufferBuilder.end();
+        BufferRenderer.drawWithGlobalProgram(buffer);
+    }
+}
